fd2a329215b216425da6e809ecb68b3021d64d9c
JuliaVesel <jvesel43@bk.ru>
2020-12-09 15:56:37 +0300

    Перенесла все в отдельный пакет

--- src/classes/Setting.java
+++ src/classes/Setting.java
@@ -1,3 +1,5 @@
+package classes;
+
 import interfaces.ISetting;
 
 public class Setting implements ISetting {
--- src/classes/User.java
+++ src/classes/User.java
@@ -1,3 +1,5 @@
+package classes;
+
 import java.util.ArrayList;
 import interfaces.IList;
 import interfaces.ISetting;
